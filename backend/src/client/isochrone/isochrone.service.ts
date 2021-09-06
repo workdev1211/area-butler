3313e59d311685684644a909740ebe8aeb01269b
Kai Lehmann <kai.lehmann@syndicats.de>
2021-09-06 11:57:13 +0200

    adds error handling to isochrone service

--- backend/src/client/isochrone/isochrone.service.ts
+++ backend/src/client/isochrone/isochrone.service.ts
@@ -1,5 +1,8 @@
 import {
-  ApiCoordinates, ApiIsochrone, MeansOfTransportation, UnitsOfTransportation
+  ApiCoordinates,
+  ApiIsochrone,
+  MeansOfTransportation,
+  UnitsOfTransportation,
 } from '@area-butler-types/types';
 import { HttpService, Injectable } from '@nestjs/common';
 import { configService } from 'src/config/config.service';
@@ -9,17 +12,18 @@ import { meansOfTransportations } from '../../../../shared/constants/constants';
 export class IsochroneService {
   private baseUrl = 'https://api.mapbox.com/isochrone/v1/mapbox';
 
-  constructor(
-    private http: HttpService
-  ) {}
+  constructor(private http: HttpService) {}
 
   async fetchIsochrone(
     preferredMeansOfTransportation: MeansOfTransportation,
     coordinates: ApiCoordinates,
     limit: number,
     unit: UnitsOfTransportation,
-  ) : Promise<ApiIsochrone> {
-    const contour = unit === UnitsOfTransportation.METERS ? 'contours_meters' : 'contours_minutes';
+  ): Promise<ApiIsochrone> {
+    const contour =
+      unit === UnitsOfTransportation.METERS
+        ? 'contours_meters'
+        : 'contours_minutes';
     const routingProfile =
       meansOfTransportations.find(
         mot => mot.type === preferredMeansOfTransportation,
@@ -31,12 +35,17 @@ export class IsochroneService {
       access_token: configService.getMapBoxAccessToken(),
     };
 
-    const result = await this.http
-      .get(
-        `${this.baseUrl}/${routingProfile}/${coordinates.lng},${coordinates.lat}`,
-        { params },
-      )
-      .toPromise();
-    return result.data;
+    try {
+      const result = await this.http
+        .get(
+          `${this.baseUrl}/${routingProfile}/${coordinates.lng},${coordinates.lat}`,
+          { params },
+        )
+        .toPromise();
+      return result.data;
+    } catch (err) {
+      console.error('Error while fetching data from isochrone', err);
+      throw err;
+    }
   }
 }
