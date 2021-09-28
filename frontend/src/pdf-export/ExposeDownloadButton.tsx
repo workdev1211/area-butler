1f9b959d8019574f5ad24290fc2bfd21a12f44f1
Kai Lehmann <kai.lehmann@syndicats.de>
2021-09-28 23:01:07 +0200

    sets proper document name

--- frontend/src/pdf-export/ExposeDownloadButton.tsx
+++ frontend/src/pdf-export/ExposeDownloadButton.tsx
@@ -11,33 +11,39 @@ export interface ExposeDownloadButtonProps {
   realEstateListing: ApiRealEstateListing;
 }
 
-export const ExposeDownloadButton: React.FunctionComponent<ExposeDownloadButtonProps> = ({
-  groupedEntries,
-  transportationParams,
-  listingAddress,
-  realEstateListing
-}) => {
-  const componentRef = useRef();
-  return (
-    <div>
-      <ReactToPrint
-        documentTitle="Expose Kudiba"
-        trigger={() => (
-          <button className="btn btn-sm" onClick={() => {}}>
-            Umgebungsanalyse exportieren
-          </button>
-        )}
-        content={() => componentRef.current!}
-      />
-      <Expose
-        ref={componentRef}
-        groupedEntries={groupedEntries}
-        transportationParams={transportationParams}
-        listingAddress={listingAddress}
-        realEstateListing={realEstateListing}
-      />
-    </div>
-  );
-};
+export const ExposeDownloadButton: React.FunctionComponent<ExposeDownloadButtonProps> =
+  ({
+    groupedEntries,
+    transportationParams,
+    listingAddress,
+    realEstateListing,
+  }) => {
+    const componentRef = useRef();
+
+    const documentTitle = !!realEstateListing
+      ? `${realEstateListing.name.replace(/\s/g, "")}_AreaButler`
+      : `${listingAddress.replace(/\s/g, "").split(",")[0]}_AreaButler`;
+
+    return (
+      <div>
+        <ReactToPrint
+          documentTitle={documentTitle}
+          trigger={() => (
+            <button className="btn btn-sm" onClick={() => {}}>
+              Umgebungsanalyse exportieren
+            </button>
+          )}
+          content={() => componentRef.current!}
+        />
+        <Expose
+          ref={componentRef}
+          groupedEntries={groupedEntries}
+          transportationParams={transportationParams}
+          listingAddress={listingAddress}
+          realEstateListing={realEstateListing}
+        />
+      </div>
+    );
+  };
 
 export default ExposeDownloadButton;
