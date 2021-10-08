e8a9d580eabff97f68bbdb1fe263b784cb853b6d
Matthias Grube <matthias.grube@syndicats.de>
2021-10-08 13:45:20 +0200

    add privacy page

--- frontend/src/footer/Footer.tsx
+++ frontend/src/footer/Footer.tsx
@@ -4,13 +4,13 @@ import {kudibaCompany} from "../../../shared/constants/constants";
 
 const Footer: React.FunctionComponent = () => {
     return (
-        <div className="container mx-auto px-2 sm:px-0 pt-0 sm:pt-2 mt-auto">
-            <div className="relative flex flex-wrap items-center justify-between h-16 text-xs">
-                <div>
-                    <Link to="/impress" className="link-neutral"
-                          aria-current="page">Impressum</Link>
-                </div>
+        <div className="container mx-auto px-2 sm:px-0 pt-0 sm:pt-2 mt-auto h-16 text-xs">
+            <div className="flex h-full items-center justify-end gap-5">
                 <span className="text-gray-400">Copyright &copy; {new Date().getFullYear()} {kudibaCompany.name}. Alle Rechte vorbehalten.</span>
+                <Link to="/impress" className="link-neutral"
+                      aria-current="page">Impressum</Link>
+                <Link to="/privacy" className="link-neutral"
+                      aria-current="page">Datenschutz</Link>
             </div>
         </div>
     )
