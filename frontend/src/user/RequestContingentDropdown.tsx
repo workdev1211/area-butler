b390bcd74873bd1fc75a9c47ea21c30927e1ae84
Kai Lehmann <kai.lehmann@syndicats.de>
2021-10-26 15:39:42 +0200

    adds requestcontingent dropdown

---
+++ frontend/src/user/RequestContingentDropdown.tsx
@@ -0,0 +1,69 @@
+import useOnClickOutside from "hooks/onclickoutside";
+import { useRef, useState } from "react";
+import { ApiRequestContingent, ApiRequestContingentType } from "../../../shared/types/subscription-plan";
+
+export interface RequestContingentDropDownProps {
+  requestContingents: ApiRequestContingent[];
+}
+
+const RequestContingentDropDown: React.FunctionComponent<RequestContingentDropDownProps> =
+  ({ requestContingents = [] }) => {
+    const dropDownRef = useRef(null);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const contingentTypeLabels = {
+        [ApiRequestContingentType.INCREASE]: 'Erhöhung',
+        [ApiRequestContingentType.RECURRENT]: 'Monatl. Kontingent'
+    }
+
+    useOnClickOutside(dropDownRef, () => menuOpen && setMenuOpen(false));
+
+    const dropDownListStyle = menuOpen
+      ? "p-2 shadow menu text-left menu-open w-96 dropdown-content bg-base-100 rounded-box overflow-y-scroll"
+      : "p-2 shadow menu text-left dropdown-content bg-base-100 rounded-box overflow-y-scroll";
+
+    return requestContingents.length > 0 ? (
+      <div ref={dropDownRef} className="dropdown">
+        <div
+          className="bordered bg-primary cursor-pointer ml-1 py-1 px-3 text-sm text-white font-bold rounded-full"
+          tabIndex={0}
+          onClick={(e) => setMenuOpen(!menuOpen)}
+        >
+          i
+        </div>
+        {menuOpen && (
+          <table tabIndex={0} className={dropDownListStyle}>
+            <thead>
+              <tr className="text-left">
+                <th className="w-48 text-left" >Art</th>
+                <th className="w-24 text-left">Menge</th>
+                <th className="w-24 text-left">Datum</th>
+              </tr>
+            </thead>
+            <tbody>
+              {requestContingents.map(
+                (requestContingent: ApiRequestContingent) => {
+                  const date = new Date(requestContingent.date!);
+
+                  return (
+                    <tr className="text-left"
+                      key={
+                        "request-contingent-drop-down-" + requestContingent.date
+                      }
+                    >
+                      <td className="w-48 text-left">{contingentTypeLabels[requestContingent.type]}</td>
+                      <td className="w-24 text-left">{requestContingent.amount}</td>
+                      <td className="w-24 text-left">
+                        {date.getFullYear()}-{date.getMonth()}
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        )}
+      </div>
+    ) : null;
+  };
+
+export default RequestContingentDropDown;
--- frontend/src/user/SubscriptionPlanLimits.tsx
+++ frontend/src/user/SubscriptionPlanLimits.tsx
@@ -1,62 +1,68 @@
-import {FunctionComponent} from "react";
-import {ApiRealEstateListing} from "../../../shared/types/real-estate";
-import {ApiUser} from "../../../shared/types/types";
-import {allSubscriptionTypes,} from "../../../shared/constants/subscription-plan";
+import { FunctionComponent } from "react";
+import { ApiRealEstateListing } from "../../../shared/types/real-estate";
+import { ApiUser } from "../../../shared/types/types";
+import { allSubscriptionTypes } from "../../../shared/constants/subscription-plan";
+import RequestContingentDropDown from "./RequestContingentDropdown";
 
 export interface SubscriptionPlanLimitsProps {
-    user: ApiUser;
-    realEstates: ApiRealEstateListing[];
+  user: ApiUser;
+  realEstates: ApiRealEstateListing[];
 }
 
 const SubscriptionPlanLimits: FunctionComponent<SubscriptionPlanLimitsProps> =
-    ({user, realEstates}) => {
-        const subscriptionLabel =
-            allSubscriptionTypes.find(
-                (item) => user.subscriptionPlan.type === item.type
-            )?.label || "Unbekannt";
+  ({ user, realEstates }) => {
+    const subscriptionLabel =
+      allSubscriptionTypes.find(
+        (item) => user.subscriptionPlan.type === item.type
+      )?.label || "Unbekannt";
 
+    const totalRequestContingent =
+      user.requestContingents.length > 0
+        ? user.requestContingents
+            .map((c) => c.amount)
+            .reduce((acc, inc) => acc + inc)
+        : 0;
 
-        const totalRequestContingent = user.requestContingents.length > 0 ? user.requestContingents.map(c => c.amount).reduce((acc, inc) => acc + inc) : 0;
-
-        return (
-            <div className="mt-20 flex flex-col gap-5">
-                <div>
-                    <h1 className="font-bold text-xl">
-                        Ihr aktuelles Abonnement und Ihre Kontingente
-                    </h1>
-                    <h3 className="font-bold">
-                        Aktuelles Abonnement: {subscriptionLabel}
-                    </h3>
-                </div>
-                <div
-                    key="request-contingent"
-                    className="flex flex-wrap gap-6 items-center"
-                >
-          <span className="w-64">
+    return (
+      <div className="mt-20 flex flex-col gap-5">
+        <div>
+          <h1 className="font-bold text-xl">
+            Ihr aktuelles Abonnement und Ihre Kontingente
+          </h1>
+          <h3 className="font-bold">
+            Aktuelles Abonnement: {subscriptionLabel}
+          </h3>
+        </div>
+        <div
+          key="request-contingent"
+          className="flex flex-wrap gap-6 items-center"
+        >
+          <span className="w-64 flex items-center">
             Anfragen ausgeführt {user.requestsExecuted}/{totalRequestContingent}:
+            <RequestContingentDropDown requestContingents={user.requestContingents}></RequestContingentDropDown>
           </span>
-                    <progress
-                        value={user.requestsExecuted}
-                        max={totalRequestContingent}
-                        className="w-96 progress progress-primary"
-                    ></progress>
-                </div>
-                <div
-                    key="real-estate-contingent"
-                    className="flex flex-wrap gap-6 items-center"
-                >
+          <progress
+            value={user.requestsExecuted}
+            max={totalRequestContingent}
+            className="w-96 progress progress-primary"
+          ></progress>
+        </div>
+        <div
+          key="real-estate-contingent"
+          className="flex flex-wrap gap-6 items-center"
+        >
           <span className="w-64">
             Objekte angelegt {realEstates.length}/
-              {user.subscriptionPlan.limits.numberOfRealEstates}:
+            {user.subscriptionPlan.limits.numberOfRealEstates}:
           </span>
-                    <progress
-                        value={realEstates.length}
-                        max={user.subscriptionPlan.limits.numberOfRealEstates}
-                        className="w-96 progress progress-primary"
-                    ></progress>
-                </div>
-            </div>
-        );
-    };
+          <progress
+            value={realEstates.length}
+            max={user.subscriptionPlan.limits.numberOfRealEstates}
+            className="w-96 progress progress-primary"
+          ></progress>
+        </div>
+      </div>
+    );
+  };
 
 export default SubscriptionPlanLimits;
