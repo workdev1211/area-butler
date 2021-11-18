95d243d20d3164d1de3623f84e0346fb9a7be3d1
Kai Lehmann <kai.lehmann@syndicats.de>
2021-11-18 11:30:47 +0100

    adds invitate code contact option

--- frontend/src/pages/Auth0ConsentPage.tsx
+++ frontend/src/pages/Auth0ConsentPage.tsx
@@ -48,45 +48,89 @@ const Auth0ConsentPage: React.FunctionComponent = () => {
 
     const formId = `form-${uuid()}`;
     return (
-        <DefaultLayout title="Registrierung" withHorizontalPadding={true}>
-            <div className="w-1/3 mx-auto pt-20">
-                <h2>Herzlich Willkommen</h2>
-                { inviteCodeNeeded && <p className="mt-10">Die Nutzung des Area Butler steht nur einem exklusiven Nutzerkreis zur
-                    Verfügung.</p>}
-                { inviteCodeNeeded && <p>Bitte geben Sie nachfolgend Ihren Einladungscode ein.</p>}
-                <Formik
-                    initialValues={{
-                        inviteCode: '',
-                        consentGiven: false,
-                    }}
-                    validationSchema={Yup.object({
-                        inviteCode: inviteCodeNeeded ? Yup.string().required("Bitte geben Sie einen Einladungscode an.") : Yup.string(),
-                        consentGiven: Yup.boolean().oneOf([true], "Ihre Zustimmung wird benötigt.")
-                    })}
-                    onSubmit={(values) => onSubmit(values)}
-                >
-                    <Form id={formId}>
-                        {inviteCodeNeeded && <Input label="Einladungscode"
-                               name="inviteCode"
-                               type="text"
-                               placeholder="XXXXXXXX" className="input input-bordered w-full"/>}
-                        <div className="form-control mt-5">
-                            <Checkbox name="consentGiven">
-                                <div className="checkbox-container font-normal text-base">Hiermit stimme ich den <a
-                                    target="_blank" className="link-primary" href="/terms">AGB</a> und den <a
-                                    target="_blank"
-                                    className="link-primary"
-                                    href="/privacy">Datenschutzbestimmungen</a> zu.
-                                </div>
-                            </Checkbox>
-                        </div>
-                    </Form>
-                </Formik>
-                <button form={formId} key="submit" type="submit"
-                        className={busy ? 'busy btn btn-primary mt-5' : 'btn btn-primary mt-5'}>Registrieren
-                </button>
-            </div>
-        </DefaultLayout>)
+      <DefaultLayout title="Registrierung" withHorizontalPadding={true}>
+        <div className="w-1/3 mx-auto pt-20">
+          <h2>Herzlich Willkommen</h2>
+          {inviteCodeNeeded && (
+            <p className="mt-10">
+              Die Nutzung des Area Butler steht nur einem exklusiven Nutzerkreis
+              zur Verfügung.
+            </p>
+          )}
+          {inviteCodeNeeded && (
+            <p>Bitte geben Sie nachfolgend Ihren Einladungscode ein.</p>
+          )}
+          <Formik
+            initialValues={{
+              inviteCode: "",
+              consentGiven: false,
+            }}
+            validationSchema={Yup.object({
+              inviteCode: inviteCodeNeeded
+                ? Yup.string().required(
+                    "Bitte geben Sie einen Einladungscode an."
+                  )
+                : Yup.string(),
+              consentGiven: Yup.boolean().oneOf(
+                [true],
+                "Ihre Zustimmung wird benötigt."
+              ),
+            })}
+            onSubmit={(values) => onSubmit(values)}
+          >
+            <Form id={formId}>
+              {inviteCodeNeeded && (
+                <Input
+                  label="Einladungscode"
+                  name="inviteCode"
+                  type="text"
+                  placeholder="XXXXXXXX"
+                  className="input input-bordered w-full"
+                />
+              )}
+              <div className="form-control mt-5">
+                <Checkbox name="consentGiven">
+                  <div className="checkbox-container font-normal text-base">
+                    Hiermit stimme ich den{" "}
+                    <a target="_blank" className="link-primary" href="/terms">
+                      AGB
+                    </a>{" "}
+                    und den{" "}
+                    <a target="_blank" className="link-primary" href="/privacy">
+                      Datenschutzbestimmungen
+                    </a>{" "}
+                    zu.
+                  </div>
+                </Checkbox>
+              </div>
+            </Form>
+          </Formik>
+          <button
+            form={formId}
+            key="submit"
+            type="submit"
+            className={
+              busy ? "busy btn btn-primary mt-5" : "btn btn-primary mt-5"
+            }
+          >
+            Registrieren
+          </button>
+          {inviteCodeNeeded && (
+            <p className="mt-10">
+              Sie haben keinen Einladungscode?
+              {' '}
+              <a
+                className="text-primary"
+                href="mailto:info@area-butler.de?subject=Anfrage zum Area Butler&body=Hallo,%0D%0Dsoeben bin ich auf euren Area Butler gestoßen und ihr habt mein Interesse geweckt. Gerne würde ich sofort loslegen.%0D%0DBitte schickt mir doch einen Einladungscode.%0D%0DIch freue mich auf eure Antwort.%0D%0DMit freundlichen Grüßen%0D"
+              >
+                Kontaktieren Sie uns
+              </a>
+              {' '} jetzt
+            </p>
+          )}
+        </div>
+      </DefaultLayout>
+    );
 }
 
 export default Auth0ConsentPage;
