83a057f0e9b561cdcf212c764f55a2c3ea7d1d5c
Kai Lehmann <kai.lehmann@syndicats.de>
2021-11-18 15:43:04 +0100

    adds average data for census

--- frontend/src/map/CensusTable.tsx
+++ frontend/src/map/CensusTable.tsx
@@ -5,6 +5,18 @@ export interface CensusTableProps {
   censusData: ApiGeojsonFeature[];
 }
 
+const averageCensus = {
+  'Durchschnittsalter': 44,
+  'Anteil der Ausländer': 8,
+  'Einwohner': 4041,
+  'Durchschnittliche Haushaltsgröße': 2,
+  'Anteil der leerstehenden Wohnungen': 0,
+  'Durchschnittliche Wohnfläche je Bewohner': 37,
+  'Durchschnittliche Wohnfläche je Wohnung': 68,
+  'Anteil der Bevölkerung ab 65 Jahre': 22,
+  'Anteil der Bevölkerung unter 18 Jahre': 15,
+} as any;
+
 const CensusTable: FunctionComponent<CensusTableProps> = ({ censusData }) => {
   const censusCenter = censusData[0] as any;
 
@@ -14,13 +26,20 @@ const CensusTable: FunctionComponent<CensusTableProps> = ({ censusData }) => {
 
   return (
     <table className="table w-96 text-sm">
+            <thead>
+        <tr>
+          <th>Name</th>
+          <th><span>Wert</span><br/><span>(Ø Deutschland)</span></th>
+        </tr>
+      </thead>
       <tbody>
         {censusCenter.properties.map(
           (p: { label: string; value: string; unit: string }) => (
             <tr key={p.label}>
               <th>{p.label}</th>
               <td>
-                {p.value} {p.unit}
+                <span>{p.value} {p.unit}</span><br/>
+                <span>({averageCensus[p.label]}{!p.unit ? '': ' ' + p.unit})</span>
               </td>
             </tr>
           )
