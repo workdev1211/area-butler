a27deda688b6b399244f6ef197d9bb8764f894f2
Kai Lehmann <kai.lehmann@syndicats.de>
2021-12-01 23:22:49 +0100

    fixes image size in docx

--- frontend/src/export/docx/creator/image.creator.ts
+++ frontend/src/export/docx/creator/image.creator.ts
@@ -2,12 +2,16 @@ import { Paragraph, ImageRun } from "docx";
 
 export const createImage = (imageData: string) => {
   return new Paragraph({
+    spacing: {
+      before: 100,
+      after: 100
+    },
     children: [
       new ImageRun({
         data: Uint8Array.from(atob(imageData), (c) => c.charCodeAt(0)),
         transformation: {
-          width: 9000,
-          height: 5000,
+          width: 600,
+          height: 390,
         },
       }),
     ],
