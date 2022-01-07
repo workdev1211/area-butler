aa36a0b18e6a5c8253e8c8aecd9069adb0e03ea6
Nils Foßhag <nils.fosshag@syndicats.de>
2022-01-07 11:52:49 +0100

    fix: handle errors when communicating with HERE API

--- backend/src/routing/routing.controller.ts
+++ backend/src/routing/routing.controller.ts
@@ -1,9 +1,10 @@
-import {Body, Controller, Get, NotFoundException, Param, Post, Query} from '@nestjs/common';
+import {Body, Controller, Get, Logger, NotFoundException, Param, Post, Query} from '@nestjs/common';
 import {RoutingService} from "./routing.service";
 import {ApiRouteQuery} from "@area-butler-types/routing";
 
 @Controller('api/routes')
 export class RoutingController {
+    private readonly logger = new Logger(RoutingController.name);
     constructor(
         private routingService: RoutingService,
     ) {
@@ -14,16 +15,21 @@ export class RoutingController {
         @Body() query: ApiRouteQuery
     ) {
         if (query.destinations.length) {
-            return await Promise.all(query.destinations.map(async (destination) => {
-                return {
-                    coordinates: destination.coordinates,
-                    title: destination.title,
-                    routes: (await Promise.all(query.meansOfTransportation.map(async (transportType) => {
-                        const result = await this.routingService.getRoute(query.origin, destination.coordinates, transportType)
-                        return result
-                    }))).filter(value => !!value)
-                }
-            }))
+            try {
+                return await Promise.all(query.destinations.map(async (destination) => {
+                    return {
+                        coordinates: destination.coordinates,
+                        title: destination.title,
+                        routes: (await Promise.all(query.meansOfTransportation.map(async (transportType) => {
+                            const result = await this.routingService.getRoute(query.origin, destination.coordinates, transportType)
+                            return result
+                        }))).filter(value => !!value)
+                    }
+                }))
+            } catch (e) {
+                this.logger.error("Could not fetch routes", e)
+                return []
+            }
         } else {
             return []
         }
@@ -34,13 +40,18 @@ export class RoutingController {
         @Body() query: ApiRouteQuery
     ) {
         if (query.destinations.length) {
-            return await Promise.all(query.destinations.map(async (destination) => {
-                return {
-                    coordinates: destination.coordinates,
-                    title: destination.title,
-                    route:  await this.routingService.getTransitRoute(query.origin, destination.coordinates)
-                }
-            }))
+            try {
+                return await Promise.all(query.destinations.map(async (destination) => {
+                    return {
+                        coordinates: destination.coordinates,
+                        title: destination.title,
+                        route:  await this.routingService.getTransitRoute(query.origin, destination.coordinates)
+                    }
+                }))
+            } catch (e) {
+                this.logger.error("Could not fetch transit routes", e)
+                return []
+            }
         } else {
             return []
         }
