365bbc543fc7e242cc5acdb8bfb2d1bce06e652f
Matthias Grube <matthias.grube@syndicats.de>
2022-02-07 11:27:54 +0100

    fix show location default value

--- backend/src/location/mapper/embeddable-maps.mapper.ts
+++ backend/src/location/mapper/embeddable-maps.mapper.ts
@@ -24,21 +24,22 @@ const mapSearchResultSnapshot = (
   snapshot: ApiSearchResultSnapshot,
   config: ApiSearchResultSnapshotConfig,
 ): ApiSearchResultSnapshot => {
-
-  if (!config || config.showLocation) {
+  if (!config || config.showLocation !== false) {
     return snapshot;
   }
 
-  const randomizedCoordinates : ApiCoordinates = randomizeCoordinates(snapshot.location);
+  const randomizedCoordinates: ApiCoordinates = randomizeCoordinates(
+    snapshot.location,
+  );
 
-  // no location 
+  // no location
   const searchResponseWithoutLocation: ApiSearchResponse = {
     ...snapshot.searchResponse,
     centerOfInterest: {
       coordinates: randomizedCoordinates,
       address: null,
       distanceInMeters: 0,
-      entity: null
+      entity: null,
     },
   };
 
@@ -50,19 +51,17 @@ const mapSearchResultSnapshot = (
   };
 };
 
-
-const randomizeCoordinates = ({lat, lng}: ApiCoordinates) : ApiCoordinates => {
+const randomizeCoordinates = ({ lat, lng }: ApiCoordinates): ApiCoordinates => {
   const d1 = randomInt() / 10000;
   const d2 = randomInt() / 10000;
   return {
-    lat: (lat + d1),
-    lng: (lng + d2)
-  }
-}
-
+    lat: lat + d1,
+    lng: lng + d2,
+  };
+};
 
 const randomInt = (min = -10, max = 10) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
