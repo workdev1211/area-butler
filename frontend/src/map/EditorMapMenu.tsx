aeadada67a5568b9abdfcc48199be337d9158222
Kai Lehmann <kai.lehmann@syndicats.de>
2022-02-10 12:41:51 +0100

    adds configuration for group items and show location

--- frontend/src/map/EditorMapMenu.tsx
+++ frontend/src/map/EditorMapMenu.tsx
@@ -1,7 +1,10 @@
 import { EntityGroup } from "components/SearchResultContainer";
 import { config } from "process";
 import { useState } from "react";
-import { ApiSearchResultSnapshotConfig, ApiSearchResultSnapshotConfigTheme } from "../../../shared/types/types";
+import {
+  ApiSearchResultSnapshotConfig,
+  ApiSearchResultSnapshotConfigTheme,
+} from "../../../shared/types/types";
 import "./MapMenu.css";
 import MapMenuCollapsable from "./MapMenuCollapsable";
 
@@ -13,17 +16,25 @@ export interface EditorMapMenuProps {
 
 const EditorMapMenu: React.FunctionComponent<EditorMapMenuProps> = ({
   config,
-  onConfigChange
+  onConfigChange,
 }) => {
   const [configOptionsOpen, setConfigOptionsOpen] = useState<boolean>(true);
 
   const changeTheme = (value: ApiSearchResultSnapshotConfigTheme) => {
-    onConfigChange({...config, theme: value});
-  }
+    onConfigChange({ ...config, theme: value });
+  };
 
   const changeMapboxMap = (value: string) => {
-    onConfigChange({...config, mapBoxMapId: value});
-  }
+    onConfigChange({ ...config, mapBoxMapId: value });
+  };
+
+  const changeShowLocation = () => {
+    onConfigChange({ ...config, showLocation: !config.showLocation });
+  };
+
+  const changeGroupItems = () => {
+    onConfigChange({ ...config, groupItems: !config.groupItems });
+  };
 
   return (
     <div className="map-menu z-9000">
@@ -41,54 +52,93 @@ const EditorMapMenu: React.FunctionComponent<EditorMapMenuProps> = ({
         <div className="collapse-content">
           <ul>
             <li>
-                <div className="flex items-center gap-6 py-1">
-                  <h4 className="w-12 font-bold">Theme</h4>
-                  <label className="cursor-pointer label">
-                    <input
-                      type="radio"
-                      name="theme"
-                      checked={!config.theme || config.theme === 'DEFAULT'}
-                      onChange={() =>changeTheme('DEFAULT')}
-                      className="radio radio-sm radio-primary mr-2"
-                    />
-                    <span className="label-text">Classic</span>
-                  </label>
-                  <label className="cursor-pointer label">
-                    <input
-                      type="radio"
-                      name="theme"
-                      checked={config.theme === 'KF'}
-                      onChange={() => changeTheme('KF')}
-                      className="radio radio-sm radio-primary mr-2"
-                    />
-                    <span className="label-text">Minimalist</span>
-                  </label>
-                </div>
+              <div className="flex items-center gap-6 py-1">
+                <h4 className="w-12 font-bold">Theme</h4>
+                <label className="cursor-pointer label">
+                  <input
+                    type="radio"
+                    name="theme"
+                    checked={!config.theme || config.theme === "DEFAULT"}
+                    onChange={() => changeTheme("DEFAULT")}
+                    className="radio radio-sm radio-primary mr-2"
+                  />
+                  <span className="label-text">Classic</span>
+                </label>
+                <label className="cursor-pointer label">
+                  <input
+                    type="radio"
+                    name="theme"
+                    checked={config.theme === "KF"}
+                    onChange={() => changeTheme("KF")}
+                    className="radio radio-sm radio-primary mr-2"
+                  />
+                  <span className="label-text">Minimalist</span>
+                </label>
+              </div>
             </li>
             <li>
-                <div className="flex items-center gap-6 py-1">
-                  <h4 className="w-12 font-bold">Karte</h4>
-                  <label className="cursor-pointer label">
-                    <input
-                      type="radio"
-                      name="mapboxMap"
-                      checked={!config.mapBoxMapId || config.mapBoxMapId === 'kudiba-tech/ckvu0ltho2j9214p847jp4t4m'}
-                      onChange={() => changeMapboxMap('kudiba-tech/ckvu0ltho2j9214p847jp4t4m')}
-                      className="radio radio-sm radio-primary mr-2"
-                    />
-                    <span className="label-text">Classic</span>
-                  </label>
-                  <label className="cursor-pointer label">
-                    <input
-                      type="radio"
-                      name="mapboxMap"
-                      checked={config.mapBoxMapId === 'kudiba-tech/ckzbqgya2000414li19g3p9u1'}
-                      onChange={() => changeMapboxMap('kudiba-tech/ckzbqgya2000414li19g3p9u1')}
-                      className="radio radio-sm radio-primary mr-2"
-                    />
-                    <span className="label-text">Highlight</span>
-                  </label>
-                </div>
+              <div className="flex items-center gap-6 py-1">
+                <h4 className="w-12 font-bold">Karte</h4>
+                <label className="cursor-pointer label">
+                  <input
+                    type="radio"
+                    name="mapboxMap"
+                    checked={
+                      !config.mapBoxMapId ||
+                      config.mapBoxMapId ===
+                        "kudiba-tech/ckvu0ltho2j9214p847jp4t4m"
+                    }
+                    onChange={() =>
+                      changeMapboxMap("kudiba-tech/ckvu0ltho2j9214p847jp4t4m")
+                    }
+                    className="radio radio-sm radio-primary mr-2"
+                  />
+                  <span className="label-text">Classic</span>
+                </label>
+                <label className="cursor-pointer label">
+                  <input
+                    type="radio"
+                    name="mapboxMap"
+                    checked={
+                      config.mapBoxMapId ===
+                      "kudiba-tech/ckzbqgya2000414li19g3p9u1"
+                    }
+                    onChange={() =>
+                      changeMapboxMap("kudiba-tech/ckzbqgya2000414li19g3p9u1")
+                    }
+                    className="radio radio-sm radio-primary mr-2"
+                  />
+                  <span className="label-text">Highlight</span>
+                </label>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-6 py-1">
+                <label className="cursor-pointer label">
+                  <input
+                    type="checkbox"
+                    name="showLocation"
+                    checked={!!config.showLocation}
+                    onChange={() => changeShowLocation()}
+                    className="checkbox checkbox-sm checkbox-primary mr-2"
+                  />
+                  <span className="label-text">Objekt auf Karte anzeigen</span>
+                </label>
+              </div>
+            </li>
+            <li>
+              <div className="flex items-center gap-6 py-1">
+                <label className="cursor-pointer label">
+                  <input
+                    type="checkbox"
+                    name="showLocation"
+                    checked={!!config.groupItems}
+                    onChange={() => changeGroupItems()}
+                    className="checkbox checkbox-sm checkbox-primary mr-2"
+                  />
+                  <span className="label-text">Objekte gruppieren beim Rauszoomen</span>
+                </label>
+              </div>
             </li>
           </ul>
         </div>
