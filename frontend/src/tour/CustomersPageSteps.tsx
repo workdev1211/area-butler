9f9f0eb01ffff5bdbcfa78b424eac4e8fde69f48
Kai Lehmann <kai.lehmann@syndicats.de>
2022-03-23 21:28:47 +0100

    updates customer page tour

--- frontend/src/tour/CustomersPageSteps.tsx
+++ frontend/src/tour/CustomersPageSteps.tsx
@@ -16,31 +16,31 @@ const Steps: Step[] = [
   },
   {
     content:
-      "In dieser Tabelle werden Ihre Interessenten dargestellt mit allen relevanten Informationen.",
+      "In dieser Tabelle werden Ihre Interessenten/Zielgruppen, mit allen relevanten Informationen, dargestellt.",
     locale: defaultLocale,
     target: 'div[data-tour="customers-table"]',
   },
   {
     content:
-      "Über das Suche Symbol lässt sich eine neue Umgebungsanalyse starten. Die Daten des Interessenten werden in der Suche vorbelegt.",
+      "Über das Suche Symbol lässt sich eine neue Umgebungsanalyse starten. Die Daten des Interessenten/der Zielgruppe werden in der Suche vorbelegt.",
     locale: defaultLocale,
     target: 'img[data-tour="customers-table-item-search-button-0"]',
   },
   {
     content:
-      "Über das Stift Symbol lassen sich Interessenten editieren.",
+      "Über das Stift Symbol lassen sich Interessenten und Zielgruppen editieren.",
     locale: defaultLocale,
     target: 'img[data-tour="customers-table-item-edit-button-0"]',
   },
   {
     content:
-      "Über den Papierkorb lassen sich die Interessenten löschen.",
+      "Über den Papierkorb lassen sich die Interessenten oder Zielgruppen löschen.",
     locale: defaultLocale,
     target: 'img[data-tour="customers-table-item-delete-button-0"]',
   },
   {
     content:
-      "Über das Aktionen Menu lassen sich weitere Interessenten anlegen oder einen Fragebogen an einen neuen Interessenten versenden.",
+      "Über das Aktionen Menu können Sie weitere Interessenten oder Zielgruppen anlegen oder einen Fragebogen an einen neuen Interessenten per E-Mail versenden.",
     locale: defaultLocale,
     target: 'div[data-tour="actions-top"]',
   },
