b76735093c4f2395d44139d4e38f28d9c321ec87
vladnik <kvladnik9@gmail.com>
2022-05-31 17:43:16 +0300

    feat(#173): added loading modal on the search analysis

--- frontend/src/pages/SearchParamsPage.tsx
+++ frontend/src/pages/SearchParamsPage.tsx
@@ -1,26 +1,33 @@
+import {
+  FunctionComponent,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Form, Formik } from "formik";
+
 import FormModal, { ModalConfig } from "components/FormModal";
 import LatestUserRequestsDropDown from "components/LatestUserRequestsDropDown";
 import {
   PotentialCustomerActionTypes,
-  PotentialCustomerContext
+  PotentialCustomerContext,
 } from "context/PotentialCustomerContext";
 import {
   RealEstateActionTypes,
-  RealEstateContext
+  RealEstateContext,
 } from "context/RealEstateContext";
 import { UserContext } from "context/UserContext";
-import { Form, Formik } from "formik";
 import { useFederalElectionData } from "hooks/federalelectiondata";
 import { useParticlePollutionData } from "hooks/particlepollutiondata";
 import PotentialCustomerDropDown from "potential-customer/PotentialCustomerDropDown";
-import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import RealEstateDropDown from "real-estates/RealEstateDropDown";
 import {
   deriveAddressFromCoordinates as derivePlacesLocationFromCoordinates,
   deriveInitialEntityGroups,
   deriveTotalRequestContingent,
-  toastError
+  toastError,
 } from "shared/shared.functions";
 import TourStarter from "tour/TourStarter";
 import IncreaseRequestLimitFormHandler from "user/IncreaseRequestLimitFormHandler";
@@ -33,7 +40,7 @@ import {
   ApiSearch,
   ApiSearchResponse,
   ApiUser,
-  ApiUserRequests
+  ApiUserRequests,
 } from "../../../shared/types/types";
 import nextIcon from "../assets/icons/icons-16-x-16-outline-ic-next.svg";
 import ImportantAddresses from "../components/ImportantAddresses";
@@ -43,32 +50,35 @@ import MyLocationButton from "../components/MyLocationButton";
 import TransportationParams from "../components/TransportationParams";
 import {
   SearchContext,
-  SearchContextActionTypes
+  SearchContextActionTypes,
 } from "../context/SearchContext";
 import { useCensusData } from "../hooks/censusdata";
 import { useHttp } from "../hooks/http";
 import DefaultLayout from "../layout/defaultLayout";
+import BusyModal, { IBusyModalItem } from "../components/BusyModal";
 
-const SearchParamsPage: React.FunctionComponent = () => {
+const SearchParamsPage: FunctionComponent = () => {
   const { get, post } = useHttp();
   const { fetchNearData } = useCensusData();
   const { fetchElectionData } = useFederalElectionData();
   const { fetchParticlePollutionData } = useParticlePollutionData();
   const { userState } = useContext(UserContext);
-  const { searchContextState, searchContextDispatch } = useContext(
-    SearchContext
-  );
+  const { searchContextState, searchContextDispatch } =
+    useContext(SearchContext);
   const { potentialCustomerDispatch } = useContext(PotentialCustomerContext);
   const { realEstateDispatch, realEstateState } = useContext(RealEstateContext);
   const [newRequest, setNewRequest] = useState(true);
+  const [isShownBusyModal, setIsShownBusyModal] = useState(false);
+  const [busyModalItems, setBusyModalItems] = useState<IBusyModalItem[]>([]);
 
   useEffect(() => {
     const latestUserRequests: ApiUserRequests = userState.latestUserRequests!;
     const coordinates = searchContextState.location;
+
     if (!!coordinates) {
       setNewRequest(
         !latestUserRequests.requests.some(
-          r => JSON.stringify(r.coordinates) === JSON.stringify(coordinates)
+          (r) => JSON.stringify(r.coordinates) === JSON.stringify(coordinates)
         )
       );
     }
@@ -79,36 +89,41 @@ const SearchParamsPage: React.FunctionComponent = () => {
       const response = await get<ApiPotentialCustomer[]>(
         "/api/potential-customers"
       );
+
       potentialCustomerDispatch({
         type: PotentialCustomerActionTypes.SET_POTENTIAL_CUSTOMERS,
-        payload: response.data
+        payload: response.data,
       });
     };
+
     fetchCustomers();
-  }, [true]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     const fetchRealEstates = async () => {
       const response = await get<ApiRealEstateListing[]>(
         "/api/real-estate-listings"
       );
+
       realEstateDispatch({
         type: RealEstateActionTypes.SET_REAL_ESTATES,
-        payload: response.data
+        payload: response.data,
       });
     };
+
     fetchRealEstates();
-  }, [true]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const onLocationAutocompleteChange = (payload: any) => {
     searchContextDispatch({
       type: SearchContextActionTypes.SET_PLACES_LOCATION,
-      payload: payload.value
+      payload: payload.value,
     });
+
     if (payload.coordinates) {
       searchContextDispatch({
         type: SearchContextActionTypes.SET_LOCATION,
-        payload: payload.coordinates
+        payload: payload.coordinates,
       });
     }
   };
@@ -117,16 +132,18 @@ const SearchParamsPage: React.FunctionComponent = () => {
     searchContextDispatch({
       type: SearchContextActionTypes.SET_LOCATION,
       payload: {
-        ...coordinates
-      }
+        ...coordinates,
+      },
     });
+
     const place = (await derivePlacesLocationFromCoordinates(coordinates)) || {
       label: "Mein Standort",
-      value: { place_id: "123" }
+      value: { place_id: "123" },
     };
+
     searchContextDispatch({
       type: SearchContextActionTypes.SET_PLACES_LOCATION,
-      payload: place
+      payload: place,
     });
   };
 
@@ -140,10 +157,25 @@ const SearchParamsPage: React.FunctionComponent = () => {
     !searchContextState.location?.lat ||
     !searchContextState.location?.lng ||
     searchContextState.transportationParams.length === 0 ||
-    searchContextState.transportationParams.some(t => !t.amount) ||
+    searchContextState.transportationParams.some((t) => !t.amount) ||
     searchContextState.localityParams.length === 0;
 
-  const increaseLimitButton: React.ReactNode = (
+  const hasFederalElectionData =
+    user.subscriptionPlan?.config.appFeatures.dataSources.includes(
+      ApiDataSource.FEDERAL_ELECTION
+    );
+
+  const hasParticlePollutionData =
+    user.subscriptionPlan?.config.appFeatures.dataSources.includes(
+      ApiDataSource.PARTICLE_POLLUTION
+    );
+
+  const hasCensusData =
+    user.subscriptionPlan?.config.appFeatures.dataSources.includes(
+      ApiDataSource.CENSUS
+    );
+
+  const increaseLimitButton: ReactNode = (
     <button
       type="button"
       disabled={searchButtonDisabled}
@@ -154,31 +186,43 @@ const SearchParamsPage: React.FunctionComponent = () => {
       <img className="ml-1 -mt-0.5" src={nextIcon} alt="icon-next" />
     </button>
   );
+
   const increaseRequestLimitModalConfig: ModalConfig = {
     modalTitle: "Abfragelimit erreicht",
     buttonTitle: "Analyse starten",
     submitButtonTitle: "Neues Kontingent kaufen",
-    modalButton: increaseLimitButton
+    modalButton: increaseLimitButton,
   };
 
-  const IncreaseLimitModal: React.FunctionComponent<any> = () => (
+  const IncreaseLimitModal: FunctionComponent<any> = () => (
     <FormModal modalConfig={increaseRequestLimitModalConfig}>
       <IncreaseRequestLimitFormHandler />
     </FormModal>
   );
 
-  const SearchButton: React.FunctionComponent<any> = () => {
+  const SearchButton: FunctionComponent<any> = () => {
     const history = useHistory();
+
     const performLocationSearch = async () => {
       try {
+        setIsShownBusyModal(true);
+        const items = [];
+
+        items.push({
+          key: "location-search",
+        });
+        setBusyModalItems([...items]);
+
         searchContextDispatch({
           type: SearchContextActionTypes.SET_RESPONSE_CONFIG,
-          payload: undefined
+          payload: undefined,
         });
+
         searchContextDispatch({
           type: SearchContextActionTypes.SET_SEARCH_BUSY,
-          payload: true
+          payload: true,
         });
+
         const search: ApiSearch = {
           searchTitle:
             searchContextState?.placesLocation?.label || "Mein Standort",
@@ -187,55 +231,67 @@ const SearchParamsPage: React.FunctionComponent = () => {
           meansOfTransportation: searchContextState.transportationParams,
           preferredAmenities: searchContextState.localityParams.map(
             (l: ApiOsmEntity) => l.name
-          )
+          ),
         };
+
         const result = await post<ApiSearchResponse>(
           "/api/location/search",
           search
         );
+
         searchContextDispatch({
           type: SearchContextActionTypes.SET_SEARCH_RESPONSE,
-          payload: result.data
+          payload: result.data,
         });
-        if (
-          user.subscriptionPlan?.config.appFeatures.dataSources.includes(
-            ApiDataSource.FEDERAL_ELECTION
-          )
-        ) {
+
+        if (hasFederalElectionData) {
+          items.push({
+            key: "fetch-election-data",
+          });
+          setBusyModalItems([...items]);
+
           const federalElectionData = await fetchElectionData(
             searchContextState.location!
           );
+
           searchContextDispatch({
             type: SearchContextActionTypes.SET_FEDERAL_ELECTION_DATA,
-            payload: federalElectionData!
+            payload: federalElectionData!,
           });
         }
-        if (
-          user.subscriptionPlan?.config.appFeatures.dataSources.includes(
-            ApiDataSource.PARTICLE_POLLUTION
-          )
-        ) {
+
+        if (hasParticlePollutionData) {
+          items.push({
+            key: "fetch-particle-pollution-data",
+          });
+          setBusyModalItems([...items]);
+
           const particlePollutionData = await fetchParticlePollutionData(
             searchContextState.location!
           );
+
           searchContextDispatch({
             type: SearchContextActionTypes.SET_PARTICLE_POLLUTION_ELECTION_DATA,
-            payload: particlePollutionData
+            payload: particlePollutionData,
           });
         }
-        if (
-          user.subscriptionPlan?.config.appFeatures.dataSources.includes(
-            ApiDataSource.CENSUS
-          )
-        ) {
+
+        if (hasCensusData) {
+          items.push({
+            key: "fetch-census-data",
+          });
+          setBusyModalItems([...items]);
+
           const zensusData = await fetchNearData(searchContextState.location!);
+
           searchContextDispatch({
             type: SearchContextActionTypes.SET_ZENSUS_DATA,
-            payload: zensusData!
+            payload: zensusData!,
           });
         }
+
         searchContextDispatch({
-          type: SearchContextActionTypes.CLEAR_MAP_CLIPPINGS
+          type: SearchContextActionTypes.CLEAR_MAP_CLIPPINGS,
         });
 
         let filteredRealEstateListings;
@@ -255,19 +311,24 @@ const SearchParamsPage: React.FunctionComponent = () => {
             undefined,
             filteredRealEstateListings,
             searchContextState.preferredLocations
-          )
+          ),
         });
+
         history.push("/search-result");
       } catch (error) {
         toastError(
           "Fehler bei der Suchausführung. Bitte zu einem späteren Zeitpunkt wiederholen."
         );
+
         console.error(error);
       } finally {
         searchContextDispatch({
           type: SearchContextActionTypes.SET_SEARCH_BUSY,
-          payload: false
+          payload: false,
         });
+
+        setIsShownBusyModal(false);
+        setBusyModalItems([]);
       }
     };
 
@@ -289,6 +350,12 @@ const SearchParamsPage: React.FunctionComponent = () => {
     );
   };
 
+  const totalBusyModalItems =
+    +(hasFederalElectionData || 0) +
+    +(hasParticlePollutionData || 0) +
+    +(hasCensusData || 0) +
+    1;
+
   return (
     <DefaultLayout
       title="Umgebungsanalyse"
@@ -298,10 +365,18 @@ const SearchParamsPage: React.FunctionComponent = () => {
           <IncreaseLimitModal key="search-button" />
         ) : (
           <SearchButton key="search-button" />
-        )
+        ),
       ]}
     >
       <TourStarter tour="search" />
+      {isShownBusyModal && (
+        <BusyModal
+          items={busyModalItems}
+          totalItems={totalBusyModalItems}
+          isAnimated={true}
+          isRandomMessages={true}
+        />
+      )}
       <Formik initialValues={{ lat: "", lng: "" }} onSubmit={() => {}}>
         <Form>
           <h2>Lage</h2>
@@ -327,10 +402,10 @@ const SearchParamsPage: React.FunctionComponent = () => {
           <div className="sub-content">
             <TransportationParams
               values={searchContextState.transportationParams}
-              onChange={newParams =>
+              onChange={(newParams) =>
                 searchContextDispatch({
                   type: SearchContextActionTypes.SET_TRANSPORTATION_PARAMS,
-                  payload: newParams
+                  payload: newParams,
                 })
               }
             />
@@ -340,10 +415,10 @@ const SearchParamsPage: React.FunctionComponent = () => {
           <div className="sub-content">
             <ImportantAddresses
               inputValues={searchContextState.preferredLocations}
-              onChange={importantAdresses =>
+              onChange={(importantAdresses) =>
                 searchContextDispatch({
                   type: SearchContextActionTypes.SET_PREFERRED_LOCATIONS,
-                  payload: importantAdresses
+                  payload: importantAdresses,
                 })
               }
             />
@@ -351,10 +426,10 @@ const SearchParamsPage: React.FunctionComponent = () => {
           <h2>Lokalitäten</h2>
           <LocalityParams
             values={searchContextState.localityParams}
-            onChange={newValues =>
+            onChange={(newValues) =>
               searchContextDispatch({
                 type: SearchContextActionTypes.SET_LOCALITY_PARAMS,
-                payload: newValues
+                payload: newValues,
               })
             }
           />
