372bd9c9c84e7a46ece266ff4bf6f0fd8489c1e8
vladnik <kvladnik9@gmail.com>
2022-08-30 17:56:34 +0300

    chore(main): increased json body size to 10mb

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { json } from 'body-parser';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json } from 'body-parser';
 const cloneBuffer = require('clone-buffer');
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ exposedHeaders: ['Content-Disposition'] });
+
   app.use(
     json({
-      limit: '5mb',
+      limit: '10mb',
       verify: (req: any, res, buf, encoding) => {
         if (req.headers['stripe-signature'] && Buffer.isBuffer(buf)) {
           req.rawBody = cloneBuffer(buf);
         }
+
         return true;
       },
     }),
@@ -28,10 +31,13 @@ async function bootstrap() {
       .setVersion('1.0')
       .addTag('areabutler')
       .build();
+
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
   }
-  app.useGlobalPipes(new ValidationPipe({forbidUnknownValues: true}));
+
+  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }));
   await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
 }
+
 bootstrap();
