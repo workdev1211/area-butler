610388571f6f5e797edffcc1a4ecbadd7de36ed9
vladnik <kvladnik9@gmail.com>
2022-11-25 07:55:56 +0200

    feat(export): added new customer elements

--- frontend/src/map/menu/export-tab/ExportTab.tsx
+++ frontend/src/map/menu/export-tab/ExportTab.tsx
@@ -30,6 +30,8 @@ import mapScreenshotsIcon from "../../../assets/icons/map-menu/07-kartenausschni
 import digitalMediaIcon from "../../../assets/icons/map-menu/08-digitale-medien.svg";
 import reportsIcon from "../../../assets/icons/map-menu/09-reporte.svg";
 import aiDescriptionIcon from "../../../assets/icons/map-menu/10-ki-lagetexte.svg";
+import webLinkIcon from "../../../assets/icons/link.svg";
+import fileIcon from "../../../assets/icons/file.svg";
 import { UserActionTypes, UserContext } from "../../../context/UserContext";
 import ExportModal, { ExportTypeEnum } from "../../../export/ExportModal";
 import OnePageExportModal from "../../../export/one-page/OnePageExportModal";
@@ -56,6 +58,8 @@ const ExportTab: FunctionComponent<IExportTabProps> = ({
   const [isDigitalMediaOpen, setIsDigitalMediaOpen] = useState(false);
   const [isReportsOpen, setIsReportsOpen] = useState(true);
   const [isAiDescriptionOpen, setIsAiDescriptionOpen] = useState(false);
+  const [isCustomerLinksOpen, setIsCustomerLinksOpen] = useState(false);
+  const [isCustomerDataOpen, setIsCustomerDataOpen] = useState(false);
 
   useEffect(() => {
     if (
@@ -457,6 +461,82 @@ const ExportTab: FunctionComponent<IExportTabProps> = ({
             </div>
           </div>
         )}
+
+        <div
+          className={
+            "collapse collapse-arrow view-option" +
+            (isCustomerLinksOpen ? " collapse-open" : " collapse-closed")
+          }
+        >
+          <div
+            className="collapse-title"
+            ref={(node) => {
+              setBackgroundColor(node, backgroundColor);
+            }}
+            onClick={() => {
+              setIsCustomerLinksOpen(!isCustomerLinksOpen);
+            }}
+          >
+            <div className="collapse-title-container">
+              <img src={webLinkIcon} alt="customer-links-icon" />
+              <div className="collapse-title-text">
+                <div className="collapse-title-text-1">Eigene Links</div>
+                <div className="collapse-title-text-2">
+                  Hyperlinks zu externen Quellen oder Diensten
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="collapse-content">
+            <div
+              className="text-justify"
+              style={{
+                padding:
+                  "var(--menu-item-pt) var(--menu-item-pr) var(--menu-item-pb) var(--menu-item-pl)",
+              }}
+            >
+              Hier könnten Ihre Links aufgeführt werden.
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={
+            "collapse collapse-arrow view-option" +
+            (isCustomerDataOpen ? " collapse-open" : " collapse-closed")
+          }
+        >
+          <div
+            className="collapse-title"
+            ref={(node) => {
+              setBackgroundColor(node, backgroundColor);
+            }}
+            onClick={() => {
+              setIsCustomerDataOpen(!isCustomerDataOpen);
+            }}
+          >
+            <div className="collapse-title-container">
+              <img src={fileIcon} alt="customer-data-icon" />
+              <div className="collapse-title-text">
+                <div className="collapse-title-text-1">Eigene Dateien</div>
+                <div className="collapse-title-text-2">
+                  Dokumente zum Objekt speichern
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="collapse-content">
+            <div
+              className="text-justify"
+              style={{
+                padding:
+                  "var(--menu-item-pt) var(--menu-item-pr) var(--menu-item-pb) var(--menu-item-pl)",
+              }}
+            >
+              Hier könnten Sie Ihre Dateien ablegen.
+            </div>
+          </div>
+        </div>
       </div>
 
       {exportType && exportType !== ExportTypeEnum.ONE_PAGE && (
