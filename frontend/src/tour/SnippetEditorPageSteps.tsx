3ab39380ac7cc205f39266f0a03ed2adcfacb0e6
AlexTimper <alexandertimper@gmail.com>
2022-12-07 10:50:21 +0000

    Update frontend/src/tour/SnippetEditorPageSteps.tsx

--- frontend/src/tour/SnippetEditorPageSteps.tsx
+++ frontend/src/tour/SnippetEditorPageSteps.tsx
@@ -90,7 +90,7 @@ const Steps: Step[] = [
   },
   {
     content:
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/teRhSH2w0f4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+      "<iframe width="560" height="315" src="https://www.youtube.com/embed/teRhSH2w0f4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>",
     locale: defaultLocale,
     placement: "center",
     target: "body",
