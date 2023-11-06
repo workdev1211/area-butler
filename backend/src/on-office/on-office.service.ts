c6d39c3645bd59c8e14eae71b34f79979337dde1
vladnik <vladimir.kuznetsov@brocoders.team>
2023-11-06 14:42:47 +0200

    fix(snapshot): added some snapshot loggers to trace the map icon issue

--- backend/src/on-office/on-office.service.ts
+++ backend/src/on-office/on-office.service.ts
@@ -217,6 +217,13 @@ export class OnOfficeService {
     let integrationUser: TIntegrationUserDocument;
 
     if (existingUser) {
+      this.logger.debug(
+        this.login.name,
+        11,
+        integrationUserId,
+        existingUser.integrationUserId,
+      );
+
       const { userName, email } = await this.fetchUserData({
         ...existingUser.parameters,
         extendedClaim,
@@ -254,6 +261,8 @@ export class OnOfficeService {
     }
 
     if (!integrationUser && existingUser) {
+      this.logger.debug(this.login.name, 22, existingUser.integrationUserId);
+
       const { userName, email } = await this.fetchUserData({
         ...existingUser.parameters,
         extendedClaim,
@@ -314,6 +323,15 @@ export class OnOfficeService {
       integrationUser,
       estateId,
       this.integrationType,
+    );
+
+    this.logger.debug(
+      this.login.name,
+      33,
+      integrationUser.integrationUserId,
+      integrationUser.config.logo?.slice(0, 100),
+      integrationUser.config.color,
+      integrationUser.config.mapIcon?.slice(0, 100),
     );
 
     return {
@@ -931,6 +949,16 @@ export class OnOfficeService {
       "User color and logo haven't been retrieved!",
       request,
       response,
+    );
+
+    this.logger.debug(
+      this.fetchLogoAndColor.name,
+      response.response.results[0].data.records[0].elements.basicData.characteristicsCi?.logo?.slice(
+        0,
+        100,
+      ),
+      response.response.results[0].data.records[0].elements.basicData
+        .characteristicsCi?.color,
     );
 
     return response.response.results[0].data.records[0].elements.basicData
