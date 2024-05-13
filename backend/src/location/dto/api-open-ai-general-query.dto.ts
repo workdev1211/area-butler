7161dee0cea160f7a14539a1a8c8aed91d8b1537
Nils Ernsting <nils@ernsting.io>
2024-05-13 21:02:35 +0200

    hotfix(open-ai): validate general form values for open ai queries in the backend

--- backend/src/location/dto/api-open-ai-general-query.dto.ts
+++ backend/src/location/dto/api-open-ai-general-query.dto.ts
@@ -5,20 +5,26 @@ import {
   OpenAiTextLengthEnum,
   OpenAiTonalityEnum,
 } from '@area-butler-types/open-ai';
+import { Exclude, Expose } from 'class-transformer';
 
+@Exclude()
 class ApiOpenAiGeneralQueryDto implements IOpenAiGeneralFormValues {
+  @Expose()
   @IsOptional()
   @IsEnum(OpenAiTonalityEnum)
   tonality?: OpenAiTonalityEnum;
 
+  @Expose()
   @IsOptional()
   @IsString()
   targetGroupName?: string;
 
+  @Expose()
   @IsOptional()
   @IsString()
   customText?: string;
 
+  @Expose()
   @IsOptional()
   @IsEnum(OpenAiTextLengthEnum)
   textLength?: OpenAiTextLengthEnum;
